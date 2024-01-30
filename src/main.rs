9413bc6e50ee62c1cef0878771aebc61c2628ba2
jiebaef <github.sfzfba@jiebaef.dev>
2024-01-30 19:56:40 +0100

    example get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use reqwest::blocking::get;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let response = get("https://google.com")?;
+
+    if response.status().is_success() {
+        let body = response.text()?;
+        println!("Response body: {body}")
+    } else {
+        eprintln!("Request failed with status: {}", response.status())
+    }
+
+    Ok(())
 }
